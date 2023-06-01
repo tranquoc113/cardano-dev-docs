db269e13212c070368a18028840502d47ef2b747
tranquoc113 <tranquoc631@gmail.com>
2023-06-02 00:43:30 +0700

    update introduction

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from 'next/image'
+// import Link from 'next/link'
 
 const config: DocsThemeConfig = {
-  logo: <span>Cardano Developer</span>,
+  logo: <span style={{display: 'contents'}}><Image src="/logo.jpg" width={30} height={10} alt='logo'/> <span>Cardano Marlowe</span></span>,
+  logoLink: "/",
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/input-output-hk/marlowe/tree/master/papers',
   },
   chat: {
-    link: 'https://discord.com',
+    icon: <Image 
+    alt='Alote'
+    width={50}
+    height={50}
+    src="/telegram.png"
+    />,
+    link: 'https://t.me/cardanomarlowevietnam',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/input-output-hk/marlowe',
   footer: {
-    text: 'UpdateGroup Docs Template',
+    text: 'Marlowe Docs Việt Nam',
   },
+  gitTimestamp: <span>Date: {new Date().getDay() + ' - '+ new Date().getMonth() +' - ' +new Date().getUTCFullYear()}</span>
 }
 
 export default config
